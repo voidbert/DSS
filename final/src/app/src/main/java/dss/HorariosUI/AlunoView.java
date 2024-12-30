5302a186efa6c485fe048b07423619901670175f
José Lopes <jlopesum004@gmail.com>
2024-12-30 18:32:54 +0000

    Create Aluno view and controller

---
+++ final/src/app/src/main/java/dss/HorariosUI/AlunoView.java
@@ -0,0 +1,37 @@
+package dss.HorariosUI;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import dss.HorariosLN.LNException;
+
+public class AlunoView implements View{
+    private AlunoController controlador;
+
+    public AlunoView(AlunoController controlador) {
+        this.controlador = controlador;
+    }
+
+    private void visualizarHorario() {
+        try {
+            this.controlador.visualizarHorario();
+        } catch (LNException e){
+            System.err.println(e);
+        }
+    }
+
+    public View run() {
+        boolean[]   exitRequest = { false }; // Array wrapper to allow for lambda modification
+        MenuEntry[] entries =
+            {new MenuEntry("Visualizar Horário", i -> {this.visualizarHorario();}),
+            new MenuEntry("Voltar atrás", i -> { exitRequest[0] = true;})};
+
+        try {
+            do {
+                new Menu(entries, new Scanner(System.in)).run();
+            } while (!exitRequest[0]);
+        } catch (NoSuchElementException e) {} // System.in closed
+
+        return new IniciarSessaoView(new IniciarSessaoController(this.controlador.getModelo()));
+    }
+}
