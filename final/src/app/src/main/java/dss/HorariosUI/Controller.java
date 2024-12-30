4df6dce3aaffeb9f026148b52c93454ee11cbc02
José Lopes <jlopesum004@gmail.com>
2024-12-30 18:35:39 +0000

    Make Controller and View generic

--- final/src/app/src/main/java/dss/HorariosUI/Controller.java
+++ final/src/app/src/main/java/dss/HorariosUI/Controller.java
@@ -1,16 +1,9 @@
 package dss.HorariosUI;
 
 import dss.HorariosLN.IHorariosLN;
-import dss.HorariosLN.LNException;
 
-public class Controller {
-    IHorariosLN model;
-
-    public Controller(IHorariosLN m) {
-        this.model = m;
-    }
-
-    public void iniciarSessao(String email, String password) throws LNException {
-        this.model.iniciarSessao(email, password);
-    }
+public abstract class Controller {
+    private IHorariosLN modelo;
+    public Controller(IHorariosLN modelo) { this.modelo = modelo; }
+    public IHorariosLN getModelo() { /* Clone model ? no */ return this.modelo; }
 }
