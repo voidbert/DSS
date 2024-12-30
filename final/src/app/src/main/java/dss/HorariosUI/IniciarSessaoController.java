f9e9c0618c214dda360a5928d2f1dff677389118
José Lopes <jlopesum004@gmail.com>
2024-12-30 21:33:11 +0000

    set nextView to the correct view

--- final/src/app/src/main/java/dss/HorariosUI/IniciarSessaoController.java
+++ final/src/app/src/main/java/dss/HorariosUI/IniciarSessaoController.java
@@ -15,4 +15,13 @@ public class IniciarSessaoController extends Controller {
     public void terminarSessao() throws LNException {
         this.terminarSessao();
     }
+
+    public View nextView() {
+        try {
+            this.getModelo().obterNumeroAlunoAutenticado();
+            return new AlunoView(new AlunoController(this.getModelo()));
+        } catch (LNException e) {
+            return new DiretorCursoView(new DiretorCursoController(this.getModelo()));
+        }
+    }
 }
