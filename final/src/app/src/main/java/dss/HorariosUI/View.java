2466bf4bc39a040525a25d540457235dcbe5b861
José Lopes <jlopesum004@gmail.com>
2024-12-30 18:35:39 +0000

    Make Controller and View generic

--- final/src/app/src/main/java/dss/HorariosUI/View.java
+++ final/src/app/src/main/java/dss/HorariosUI/View.java
@@ -1,38 +1,5 @@
 package dss.HorariosUI;
 
-import java.util.NoSuchElementException;
-
-import dss.HorariosLN.LNException;
-
-public class View {
-    Controller controller;
-
-    public View(Controller c) {
-        this.controller = c;
-    }
-
-    public void iniciarSessao() {
-        UserInput input = new UserInput();
-        String email = input.readString("Email > ");
-        String password = input.readString("Palavra passe > ");
-
-        try {
-            this.controller.iniciarSessao(email, password);
-            System.out.println("Sessão iniciada com sucesso!");
-        } catch (LNException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-    public void run() {
-        boolean[]   exitRequest = { false }; // Array wrapper to allow for lambda modification
-        MenuEntry[] entries = {new MenuEntry("Iniciar Sessão", i -> {this.iniciarSessao();}),
-                            new MenuEntry("Sair", i -> { exitRequest[0] = true;})};
-
-        try {
-            do {
-                new Menu(entries).run();
-            } while (!exitRequest[0]);
-        } catch (NoSuchElementException e) {} // System.in closed
-    }
+public interface View {
+    public abstract View run();
 }
