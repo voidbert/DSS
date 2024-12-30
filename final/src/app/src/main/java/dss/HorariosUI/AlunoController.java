9a7b61bf11be4fdca4dd5f5c0ab5a0c662aa9852
José Lopes <jlopesum004@gmail.com>
2024-12-30 18:32:54 +0000

    Create Aluno view and controller

---
+++ final/src/app/src/main/java/dss/HorariosUI/AlunoController.java
@@ -0,0 +1,14 @@
+package dss.HorariosUI;
+
+import dss.HorariosLN.IHorariosLN;
+import dss.HorariosLN.LNException;
+
+public class AlunoController extends Controller {
+    public AlunoController(IHorariosLN modelo) {
+        super(modelo);
+    }
+
+    public void visualizarHorario() throws LNException {
+        throw new LNException("Not yet implemented!");
+    }
+}
