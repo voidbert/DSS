6ec3a0a83169fe7509ad8c22b043b788bb677f03
José Lopes <jlopesum004@gmail.com>
2025-01-02 17:54:16 +0000

    incomplete implementation of some remaining methods

--- final/src/app/src/main/java/dss/HorariosUI/DiretorCursoController.java
+++ final/src/app/src/main/java/dss/HorariosUI/DiretorCursoController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import dss.HorariosLN.IHorariosLN;
 import dss.HorariosLN.LNException;
+import dss.HorariosLN.SubSistemaHorarios.Horario;
 
 public class DiretorCursoController extends Controller {
     public DiretorCursoController(IHorariosLN modelo) {
@@ -30,7 +31,9 @@ public class DiretorCursoController extends Controller {
         this.getModelo().importarUCs(caminhoFicheiro, idCurso);
     }
 
-    public void importarAlunosEInscricoes() {
+    public void importarAlunosEInscricoes() throws LNException {
+        String idCurso = this.getModelo().obterIdCursoDiretorAutenticado();
+        this.getModelo().importarAlunos("/home/sushe/Área de Trabalho/uni/2_year/dss/DSS2425-Grupo-13/data/alunos.json", idCurso);
     }
 
     public void definirPreferenciasUC() {
@@ -39,12 +42,32 @@ public class DiretorCursoController extends Controller {
     public void adicionarAluno() {
     }
 
-    public void gerarHorariosAutomaticamente() {
+    public void gerarHorariosAutomaticamente() throws LNException {
+        String idCurso = this.getModelo().obterIdCursoDiretorAutenticado();
+        throw new LNException("Not implemented yet");
+
+    }
+
+    public Horario obterHorarioAluno(String numAluno) throws LNException {
+        String idCurso = this.getModelo().obterIdCursoDiretorAutenticado();
+
+        if (!this.getModelo().verificarSeAlunoInscritoEmCurso(numAluno, idCurso)) {
+            throw new LNException("Aluno não se encontra inscrito num curso");
+        }
+
+        return this.getModelo().obterHorario(numAluno);
     }
 
     public void modificarHorario() {
     }
 
-    public void publicarHorarios() {
+    public Collection<String> publicarHorarios() throws LNException {
+        String idCurso = this.getModelo().obterIdCursoDiretorAutenticado();
+        Collection<String> alunos = this.getModelo().obterAlunosDeCurso(idCurso);
+
+        this.getModelo().gerarCredenciaisDeAlunos(alunos);
+        Collection<String> falhas = this.getModelo().notificarAlunos(alunos);
+
+        return falhas;
     }
 }
